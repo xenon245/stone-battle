9cd34ab855f7384ef287c96dda5639ddb551d333
sonogram70 <sonogram70@gmail.com>
2021-04-12 10:59:43 +0900

    Finished!

--- src/main/kotlin/com/github/monulo/stone/Stone.kt
+++ src/main/kotlin/com/github/monulo/stone/Stone.kt
@@ -2,8 +2,14 @@ package com.github.monulo.stone
 
 import com.github.monun.tap.fake.FakeEntityServer
 import com.github.monun.tap.fake.FakeProjectileManager
+import org.bukkit.Bukkit
+import org.bukkit.boss.BossBar
 
 object Stone {
     lateinit var fakeEntityServer: FakeEntityServer
     lateinit var fakeProjectileManager: FakeProjectileManager
+    var redb = Bukkit.getServer().worlds.first().getBlockAt(-75, 9, 6)
+    var blueb = Bukkit.getServer().worlds.first().getBlockAt(91, 9, 10)
+    lateinit var red: BossBar
+    lateinit var blue: BossBar
 }
